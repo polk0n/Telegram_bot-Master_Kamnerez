48835527df85ffb3a5e200bc28619cce82f10720
Admin <pol.k0n@yandex.ru>
2023-02-18 15:46:30 +0600

    07 Changed model initialization
    model initialization rebuilt in such a way that it starts from the main file

--- db/models.py
+++ db/models.py
@@ -1,6 +1,3 @@
-import os
-import asyncio
-from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import (
     Column,
@@ -8,7 +5,6 @@ from sqlalchemy import (
     String,
 )
 
-engine = create_async_engine(f"postgresql+asyncpg://postgres:{os.getenv('PSQL_PASS')}@localhost/kamnerez_db")
 Base = declarative_base()
 
 
@@ -27,11 +23,8 @@ class Product(Base):
         return f"{self.product_id}, {self.kind}, {self.material}, {self.size}, {self.photo}"
 
 
-async def models_init():
+async def models_init(engine):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-
-
-if __name__ == "__main__":
-    asyncio.run(models_init())
+        print("Tables have been initialized")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine
 from create_bot import dp, bot
 import commands
 import handlers
 from db.create_base import connect_database
+from db.models import models_init
 
 commands.register_handlers(dp)
 handlers.register_handlers(dp)
@@ -19,15 +21,13 @@ async def main():
     password=os.getenv("PSQL_PASS")) is the password from the user in the postgresql system,
     if you do not have a password, leave the field blank.
     """
-    bot_task = asyncio.create_task(bot_launch())
-    base_task = asyncio.create_task(
-        connect_database(
-            user="postgres",
-            database="kamnerez_db",
-            password=os.getenv("PSQL_PASS"))
-    )
-    await bot_task
-    await base_task
+    await connect_database(
+        user="postgres",
+        database="kamnerez_db",
+        password=os.getenv("PSQL_PASS"))
+    engine = create_async_engine(f"postgresql+asyncpg://postgres:{os.getenv('PSQL_PASS')}@localhost/kamnerez_db")
+    await models_init(engine=engine)
+    await bot_launch()
 
 
 if __name__ == "__main__":
