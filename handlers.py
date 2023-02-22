a013f0b19dbaa20883cce1acc60047ffb138d88c
Admin <pol.k0n@yandex.ru>
2023-02-22 11:50:28 +0600

    12 The handler for gallery buttons is added

--- handlers.py
+++ handlers.py
@@ -1,8 +1,10 @@
 from aiogram import Dispatcher, types
-from keyboards import keyboard_galery
+from db.base_utils import get_group_obj
+from keyboards import keyboard_galery, make_inl_keyb
 from aiogram.filters import Text
 from aiogram.fsm.context import FSMContext
-from create_bot import MyStatesGroup
+from create_bot import bot, MyStatesGroup
+from db.models import Product
 
 
 async def open_galery(msg: types.Message, state=FSMContext):
@@ -12,5 +14,35 @@ async def open_galery(msg: types.Message, state=FSMContext):
     await state.set_state(MyStatesGroup.galery)
 
 
+async def send_photo(msg: types.Message, product):
+    """
+    The function takes an object of the Product class
+    and sends a photo and description generated from the object to the chat.
+    """
+    photo = product.photo
+    caption = "".join([product.material, "\n", product.size, "\n", product.price])
+    link = product.site_link
+    await bot.send_photo(chat_id=msg.chat.id,
+                         photo=photo,
+                         caption=caption,
+                         reply_markup=make_inl_keyb(link))
+
+
+async def show_canvas(msg: types.Message, state: FSMContext):
+    """
+    The function receives a product group list, takes the first element from it
+    and passes it to the next function 'send_photo'.
+    """
+    products = await get_group_obj(Product, msg.text)
+    await state.update_data(products=products)
+    await state.update_data(product_number=0)
+    data = await state.get_data()
+    product_number = data["product_number"]
+    products = data["products"]
+    product = products[product_number]
+    await send_photo(msg, product)
+
+
 def register_handlers(dp: Dispatcher):
     dp.message.register(open_galery, Text(text="Открыть галерею"))
+    dp.message.register(show_canvas, Text(text=["Шары", "Яйца", "Шкатулки"]), MyStatesGroup.galery)
